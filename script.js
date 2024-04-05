919e2c06f1c7165cfce21ee1d66143e3bb8b35a2
Michael Francis <4574480+mfrancis95@users.noreply.github.com>
2024-04-05 18:20:28 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const shifts = [
+    new Date('2024-04-06'),
+    new Date('2024-04-07'),
+    new Date('2024-04-10'),
+    new Date('2024-04-11'),
+    new Date('2024-04-14'),
+    new Date('2024-04-15'),
+    new Date('2024-04-17'),
+    new Date('2024-04-18'),
+    new Date('2024-04-19'),
+    new Date('2024-04-20'),
+    new Date('2024-04-22'),
+    new Date('2024-04-23'),
+    new Date('2024-04-27'),
+    new Date('2024-04-28'),
+    new Date('2024-04-29')
+];
+
+const now = new Date();
+const index = shifts.findIndex(element => element >= now);
+
+document.getElementById('shifts').innerHTML = index === -1 ? shifts.length : shifts.length - index;
