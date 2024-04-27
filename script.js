dc3e69cdd6a91d4d8d8aac4ef8826a6473052c04
Michael Francis <4574480+mfrancis95@users.noreply.github.com>
2024-04-27 18:13:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,21 +17,28 @@ const emoji = emojis[Math.floor(Math.random() * emojis.length)];
 document.getElementById('emoji').innerHTML = emoji.repeat(Math.floor(Math.random() * emojis.length));
 
 const shifts = [
-    new Date('2024-04-06'),
-    new Date('2024-04-07'),
-    new Date('2024-04-10'),
-    new Date('2024-04-11'),
-    new Date('2024-04-14'),
-    new Date('2024-04-15'),
-    new Date('2024-04-17'),
-    new Date('2024-04-18'),
-    new Date('2024-04-19'),
-    new Date('2024-04-20'),
-    new Date('2024-04-22'),
-    new Date('2024-04-23'),
     new Date('2024-04-27'),
     new Date('2024-04-28'),
-    new Date('2024-04-29')
+    new Date('2024-04-29'),
+    new Date('2024-05-01'),
+    new Date('2024-05-02'),
+    new Date('2024-05-03'),
+    new Date('2024-05-04'),
+    new Date('2024-05-08'),
+    new Date('2024-05-09'),
+    new Date('2024-05-10'),
+    new Date('2024-05-11'),
+    new Date('2024-05-15'),
+    new Date('2024-05-16'),
+    new Date('2024-05-20'),
+    new Date('2024-05-21'),
+    new Date('2024-05-22'),
+    new Date('2024-05-23'),
+    new Date('2024-05-25'),
+    new Date('2024-05-26'),
+    new Date('2024-05-27'),
+    new Date('2024-05-28'),
+    new Date('2024-05-29')
 ];
 
 const now = new Date();
